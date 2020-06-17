67101a5f3784b574bfb1b5379b04ab1a5f54671c
twoheart <twoheart@vip.qq.com>
2020-06-17 14:55:01 +0800

    添加 nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon type='money'/>
       记账
     </router-link>
-    |
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon type="labels"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon type="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -24,8 +22,25 @@
 </script>
 
 <style scoped lang="scss">
-  .nav {
-    border: 1px solid red;
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.85);
+    flex-direction: row;
+    font-size: 12px;
+
+    > .item {
+      width: 33.33333%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 2px 0;
+    }
+
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
   }
 
 </style>
