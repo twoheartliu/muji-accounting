4781014cadad967f8b7b53e963d4b05df4138efc
twoheart <twoheart@vip.qq.com>
2020-06-17 18:47:42 +0800

    money 组件部分 html

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,30 @@
   <div class="nav-wrapper">
     <div class="content">
       <Layout>
-        <p>money</p>
+        <div class="tags">
+          <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+          </ul>
+          <div class="new">
+            <button>新增标签</button>
+          </div>
+        </div>
+        <div>
+          <label class="notes">
+            <span class="name">备注</span>
+            <input type="text">
+          </label>
+        </div>
+        <div>
+          <ul class="">
+            <li>支出</li>
+            <li>收入</li>
+          </ul>
+        </div>
+        <div>number pad</div>
       </Layout>
     </div>
   </div>
