44f89c36326fbbe41ea16492e85666466f0846c0
twoheart <twoheart@vip.qq.com>
2020-06-19 17:45:03 +0800

    add html

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -25,7 +25,25 @@
             <li>收入</li>
           </ul>
         </div>
-        <div>number pad</div>
+        <div class="numberPad">
+          <div class="output">100</div>
+          <div class="buttons">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>删除</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>清空</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>OK</button>
+            <button>0</button>
+            <button>.</button>
+          </div>
+        </div>
       </Layout>
     </div>
   </div>
